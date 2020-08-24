431c0074f42b7aa034b709873ed8dd553026360b
andrei.pak <andrei.pak@zmp.co.jp>
2020-08-24 13:19:51 +0900

    IZ-25 fix source and ROS_IP

--- rc110_service/systemd/rc110-ros.sh
+++ rc110_service/systemd/rc110-ros.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/@ROS_DISTRO@/setup.bash
+source /home/zmp/ros/devel/setup.bash
 export ROS_IP=$(hostname -I | cut -d' ' -f1)
+
 roscore
--- rc110_service/systemd/rc110.sh
+++ rc110_service/systemd/rc110.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-source /opt/ros/melodic/setup.bash
+source /opt/ros/@ROS_DISTRO@/setup.bash
+source /home/zmp/ros/devel/setup.bash
+export ROS_IP=$(hostname -I | cut -d' ' -f1)
+
 roslaunch --wait rc110_launch main.launch
