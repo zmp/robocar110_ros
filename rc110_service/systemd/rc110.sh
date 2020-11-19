fc609253cb608de90320a9a74fb1d359dd405280
andrei.pak <andrei.pak@zmp.co.jp>
2020-11-19 17:37:18 +0900

    IZ-63 fix ros_ip

--- rc110_service/systemd/rc110.sh
+++ rc110_service/systemd/rc110.sh
@@ -2,5 +2,19 @@
 
 source /opt/ros/@ROS_DISTRO@/setup.bash
 
-export ROS_IP=$(ifconfig "${RC110_INTERFACE}" | grep 'inet ' | awk '{print $2}')
+# Give ifconfig 10 seconds to get the ip address.
+for i in {1..10}; do
+    export ROS_IP=$(ifconfig "${RC110_INTERFACE}" | grep 'inet ' | awk '{print $2}')
+
+    if [ -n "$ROS_IP" ]; then
+        break;
+    fi
+    sleep 1
+done
+
+if [ -z "$ROS_IP" ]; then
+    echo "Failed command: ifconfig ${RC110_INTERFACE}"
+    exit 1
+fi
+
 roslaunch --wait rc110_launch robot.launch
