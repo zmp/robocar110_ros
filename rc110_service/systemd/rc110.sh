4568c5e14710e43daaa85cca92f655a1d452df42
andrei.pak <andrei.pak@zmp.co.jp>
2021-01-21 18:10:04 +0900

    RC110-30 start ros even if no network

--- rc110_service/systemd/rc110.sh
+++ rc110_service/systemd/rc110.sh
@@ -39,5 +39,26 @@ set +a
 
 echo "ROS_IP=$ROS_IP"
 
+# Disable network, if IP address is not available during 30s. If not disabled, ROS will fail to start.
+if [ -n "$ROS_IP" ]; then
+    echo "Waiting for $ROS_IP interface (up to 30s)..."
+
+    is_ip_assigned="false"
+    for i in {1..30}; do
+        if hostname -I | tr ' ' '\n' | grep -q $ROS_IP; then
+            is_ip_assigned="true"
+            break;
+        fi
+        sleep 1
+    done
+
+    if [ "$is_ip_assigned" == "false" ]; then
+        echo "Unable to find ip address $ROS_IP, so ROS_IP is disabled."
+        unset ROS_IP
+    else
+        echo "Network interface found."
+    fi
+fi
+
 # Launch ros nodes
-eval ${RC110_LAUNCH_COMMAND}
+eval "$RC110_LAUNCH_COMMAND"
